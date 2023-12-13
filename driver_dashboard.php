83d67a7266b5683dce803ec0c7f36b741f8647c6
motiullahsajit <sajitmotiullah@gmail.com>
2023-12-13 09:56:08 +0600

    session timeout addded

--- driver_dashboard.php
+++ driver_dashboard.php
@@ -1,6 +1,20 @@
 <?php
-    session_start();
+session_start();
+
+$timeout = 15 * 60;
+
+if (isset($_SESSION['last_activity_time'])) {
+    if (time() - $_SESSION['last_activity_time'] > $timeout) {
+        session_unset();
+        session_destroy();
+        header('location: logout.php');
+        exit;
+    }
+}
+
+$_SESSION['last_activity_time'] = time();
 ?>
+
 <html>
     <head>
         <title>driver dashboard</title>
